98cb187d167d963245765359f1771b8add57719b
Bharat Ratna <176641015+bharatratna@users.noreply.github.com>
2024-09-18 11:29:49 +0530

    Update Solution.sql

--- 1667. Fix Names in a Table/Solution.sql
+++ 1667. Fix Names in a Table/Solution.sql
@@ -1,9 +1,19 @@
 /* Write your T-SQL query statement below */
 
-
-
+--Incorrect Solution
+/*
 SELECT user_id,name
 
  FROM Users  
  where name =TRIM( UPPER(LEFT(name, 1)) + LOWER(Substring(name,2, LEN(name) - 1)) )
+order by user_id
+*/ 
+/* Comparing transformed name coulumn to existing name will not return correcvt output */
+
+--Correct Solution 
+SELECT user_id,
+  UPPER(LEFT(name, 1)) + LOWER(Substring(name,2, LEN(name) - 1))  as name
+
+ FROM Users  
+
 order by user_id
